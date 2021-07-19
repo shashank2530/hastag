2f3c5df198ac29b4523968ec0098fb1e383f948f
manoharc07 <manoharc07@gmail.com>
2021-07-20 01:11:59 +0530

    deploy

--- templates/index.php
+++ templates/index.php
@@ -30,6 +30,7 @@
               <h2>Hash Tags</h2>
               <p id="card_tags" class="card-text">
                 <?php
+                exec('python tag.py asdadasdada https://static.pexels.com/photos/248797/pexels-photo-248797.jpeg tags.txt');
                 $myfile = fopen("tags.txt", "r") or die("Unable to open file!");
                 echo fread($myfile,filesize("tags.txt"));
                 fclose($myfile);
